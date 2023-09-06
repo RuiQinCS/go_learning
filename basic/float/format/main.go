26b770002acf98e567252303706d5db1b8235f97
qinr <ruiqin_cs@foxmail.com>
2023-09-06 15:36:02 +0800

    feat(basic/float) : 添加截断浮点数

--- basic/float/format/main.go
+++ basic/float/format/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 )
 
@@ -25,8 +26,39 @@ func FormatFloatStr() {
 	fmt.Println(strconv.FormatFloat(v.(float64), 'f', -1, 64))
 }
 
-func main() {
-	FormatFloat()
+// prec 代表小数位数
+func TrunFloat(f float64, prec int) float64 {
+	x := math.Pow10(prec)
+	return math.Trunc(f*x) / x
+}
 
-	FormatFloatStr()
+func FormatFloatTrun() {
+	f := 2.0 / 3
+
+	f2 := TrunFloat(f, 4)
+
+	fmt.Printf("%v\n", f2)
+
+	// -1 参数表示保持原小数位数，千万要注意，如果你指定了位数就会四舍五入了
+	d2 := strconv.FormatFloat(f2, 'f', -1, 64)
+
+	fmt.Printf("%s\n", d2)
+}
+
+func FloatTrun() {
+	fmt.Println(math.Trunc(0.999))
+	fmt.Println(math.Trunc(2.199912121))
+	fmt.Println(math.Trunc(1.999212121))
+	fmt.Println(math.Trunc(0.9991212))
+	fmt.Println(math.Trunc(3.999))
+}
+
+func main() {
+	FormatFloat() // 测试fmt.Sprintf
+
+	FormatFloatStr() // 测试strconv.FormatFloat
+
+	FormatFloatTrun() // 截断浮点数格式化
+
+	FloatTrun() // 测试math.Trunc
 }
